88b8d0d8364b0660517b64ae241489f540c4f37e
M8ZOPPP <102801671+M8ZOPPP@users.noreply.github.com>
2023-03-07 20:20:40 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 #Bot Token From @BotFather
-bot_token = "Bot Token Here"
+bot_token = "6042172040:AAGjnMAuq3LaZNfugwcIUO8NiDb8Ck8_Mhs"
 
 #Admins Telegram ID
-admins = [1468386562,1914289455]
+admins = [1856249311]
 
 #MongoDb Url For Databse
-mongo_url = "Mongo Url"
+mongo_url = "mongodb+srv://mznetofficial:vsTh1Btwz7k7kaOt@cluster0.yadk0a8.mongodb.net/?retryWrites=true&w=majority"
